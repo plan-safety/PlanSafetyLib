9bc2ed9622873b885532252bf133c6331a0360a3
henry <henry@bersey.com>
2021-05-23 12:16:01 +0100

    Implemented DialogueTreeNode class

--- src/org/plansafety/lib/dialogue/DialogueTreeNode.java
+++ src/org/plansafety/lib/dialogue/DialogueTreeNode.java
@@ -1,13 +1,41 @@
 package org.plansafety.lib.dialogue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DialogueTreeNode {
 
-	private List<DialogueTreeVertex> verticies;
+	private String messageContent;
+	private final List<DialogueTreeVertex> verticies;
+
+	public DialogueTreeNode(String messageContent, DialogueTreeVertex... verticies) {
+		this.messageContent = messageContent;
+		this.verticies = new ArrayList<>(Arrays.asList(verticies));
+	}
+
+	public String getMessageContent() {
+		return this.messageContent;
+	}
+
+	public void setMessageContent(String messageContent) {
+		this.messageContent = messageContent;
+	}
+
+	public List<DialogueTreeVertex> getVerticies() {
+		return this.verticies;
+	}
+
+	public void remove(DialogueTreeVertex vertex) {
+		verticies.remove(vertex);
+	}
+
+	public void removeVertexAt(int i) {
+		verticies.remove(i);
+	}
 
 	public void append(DialogueTreeVertex vertex) {
-		throw new UnsupportedOperationException();
+		verticies.add(vertex);
 	}
 
 }
