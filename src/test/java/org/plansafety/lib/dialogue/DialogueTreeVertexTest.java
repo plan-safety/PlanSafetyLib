77dea056e43c3ed7e7f459e06d58e26830fbe8ca
henry <henry@bersey.com>
2021-05-23 18:20:06 +0100

    Added Tests for DialogueTreeVertex

---
+++ src/test/java/org/plansafety/lib/dialogue/DialogueTreeVertexTest.java
@@ -0,0 +1,42 @@
+package org.plansafety.lib.dialogue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.plansafety.lib.conversation.Message;
+import org.plansafety.lib.dialogue.criteria.IDialogueTreeCriteria;
+import org.plansafety.lib.dialogue.criteria.WordListCriteria;
+
+class DialogueTreeVertexTest {
+
+	@Test
+	void testDialogueTreeVertex() {
+		IDialogueTreeCriteria criteria = new WordListCriteria();
+		DialogueTreeNode node = new DialogueTreeNode("Hello");
+		DialogueTreeVertex vertex = new DialogueTreeVertex(criteria, node);
+
+		assertEquals(criteria, vertex.getCriteria());
+		assertEquals(node, vertex.getNode());
+	}
+
+	@Test
+	void testSetCriteria() {
+		DialogueTreeVertex vertex = new DialogueTreeVertex(null, null);
+
+		IDialogueTreeCriteria criteria = new WordListCriteria();
+		vertex.setCriteria(criteria);
+
+		assertEquals(criteria, vertex.getCriteria());
+	}
+
+	@Test
+	void testSetNode() {
+		DialogueTreeVertex vertex = new DialogueTreeVertex(null, null);
+
+		DialogueTreeNode node = new DialogueTreeNode("Hello");
+		vertex.setNode(node);
+		
+		assertEquals(node, vertex.getNode());
+	}
+
+}
