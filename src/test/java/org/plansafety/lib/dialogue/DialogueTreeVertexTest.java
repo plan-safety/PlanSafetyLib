217f964d770678c5c36f08751eb2553cb8a94af8
henry <henry@bersey.com>
2021-05-26 22:54:59 +0100

    Fixed DialogueTreeVertexTest

--- src/test/java/org/plansafety/lib/dialogue/DialogueTreeVertexTest.java
+++ src/test/java/org/plansafety/lib/dialogue/DialogueTreeVertexTest.java
@@ -3,14 +3,14 @@ package org.plansafety.lib.dialogue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.plansafety.lib.dialogue.criteria.IDialogueTreeCriteria;
+import org.plansafety.lib.dialogue.criteria.DialogueTreeCriteria;
 import org.plansafety.lib.dialogue.criteria.WordListCriteria;
 
 class DialogueTreeVertexTest {
 
 	@Test
 	void testDialogueTreeVertex() {
-		IDialogueTreeCriteria criteria = new WordListCriteria.Builder().build();
+		DialogueTreeCriteria criteria = new WordListCriteria.Builder().build();
 		DialogueTreeNode node = new DialogueTreeNode("Hello");
 		DialogueTreeVertex vertex = new DialogueTreeVertex(criteria, node);
 
@@ -22,7 +22,7 @@ class DialogueTreeVertexTest {
 	void testSetCriteria() {
 		DialogueTreeVertex vertex = new DialogueTreeVertex(null, null);
 
-		IDialogueTreeCriteria criteria = new WordListCriteria.Builder().build();
+		DialogueTreeCriteria criteria = new WordListCriteria.Builder().build();
 		vertex.setCriteria(criteria);
 
 		assertEquals(criteria, vertex.getCriteria());
